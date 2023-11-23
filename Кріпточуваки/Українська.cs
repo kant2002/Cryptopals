b17ad1ae0933a750c845c35ff7069b3d2e677d04
Andrii Kurdiumov <kant2002@gmail.com>
2023-11-23 11:12:07 +0600

    Виділив українізацію C#

---
+++ Кріпточуваки/Українська.cs
@@ -0,0 +1,67 @@
+﻿global using рядок = System.String;
+global using байт = System.Byte;
+global using ціл = System.Int32;
+global using симв = System.Char;
+global using Виключення = System.Exception;
+
+using System.Diagnostics;
+using System.Text;
+
+static class Консоль
+{
+    public static void НадрукуватиЛінію(рядок формат)
+    {
+        System.Console.WriteLine(формат);
+    }
+}
+
+class Список<Т>
+{
+    List<Т> список = new();
+
+    public void Додати(Т елемент)
+    {
+        список.Add(елемент);
+    }
+
+    public Т[] ДоМасиву() => список.ToArray();
+}
+
+class БудіникРядків
+{
+    StringBuilder будівник = new();
+
+    public void Додати(симв значення)
+    {
+        будівник.Append(значення);
+    }
+
+    public void Додати(рядок? значення)
+    {
+        будівник.Append(значення);
+    }
+
+    public void Додати(симв[]? значення)
+    {
+        будівник.Append(значення);
+    }
+
+    public рядок ДоРядка() => будівник.ToString();
+}
+
+static class Відладка
+{
+    public static void Вдостовіритися(bool умова)
+    {
+        Debug.Assert(умова);
+    }
+}
+
+static class Масив
+{
+    public static Т[] Пустий<Т>()
+    {
+        return Array.Empty<Т>();
+    }
+}
+
